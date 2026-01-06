50ba242fab7d81404fd2f41ba917d4cbed2eaf94
Abner Rodriguez <abner07282005@gmail.com>
2026-01-06 14:28:20 -0500

    Added test_data for manual testing since APOD api is down

--- nasa_client.py
+++ nasa_client.py
@@ -10,6 +10,26 @@ NASA_API_KEY = os.getenv('NASA_API_KEY')
 BASE_URL = os.getenv('BASE_URL')
 NASA_APOD_START_DATE = datetime.date(1995, 6, 16)
 
+TEST_DATA = {'resource': {
+        'image_set': "apod"
+    },
+    'concept_tags': "True",
+    'date': "2013-10-01",
+    'title': "Filaments of the Vela Supernova Remnant",
+    'url': "http://apod.nasa.gov/apod/image/1310/velafilaments_jadescope_960.jpg",
+    'explanation': ' The explosion is over but the consequences continue. About eleven thousand years ago a star in the constellation of Vela could be seen to explode. BLAH BLAH BLAH asdhjahsdhgjasd asdh.  ajsdjasdj',
+    'concepts': {
+        '0': "Astronomy",
+        '1': "Star",
+        '2': "Sun",
+        '3': "Milky Way",
+        '4': "Hubble Space Telescope",
+        '5': "Earth",
+        '6': "Nebula",
+        '7': "Interstellar medium"
+    }
+}
+
 def get_todays_apod():
     print("Getting today's apod...")
 
